a3112e0b03aa18dceefe8e670bed429944f577d0
anandvai <104161269+anandvai@users.noreply.github.com>
2024-09-09 12:32:53 +0000

    data type done

---
+++ javascript_folder/data_types.js
@@ -0,0 +1,24 @@
+let name = "vaibhav anand"
+let roll = 23
+letosOld = false
+
+
+
+
+//Number
+//String 
+//Boolean 
+//null
+//bigint
+//undefined
+//symbol
+
+
+console.log(typeof null);
+
+
+let num =3
+let num1 = -num
+console.log(num1)
+
+
---
+++ javascript_folder/variables.js
@@ -0,0 +1,15 @@
+const  accountId = 12345
+let accouuntEmail = "anandvaibhav02@gmail.com"
+var  accountpassword = 12345
+accountcity = "patna"
+// not allowed // accountId = 999
+
+console.log("hello:",accountId);
+
+accouuntEmail = 123456
+accountpassword = 123456789
+accountcity = "bengaluru"
+let product;
+
+
+console.table([accountId,accountpassword,accouuntEmail,accountcity,product]);
