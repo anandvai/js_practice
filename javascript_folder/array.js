6941ca89d0f394f159de45bc62369478699a5dc7
anandvai <104161269+anandvai@users.noreply.github.com>
2024-09-10 12:38:19 +0000

    array

---
+++ javascript_folder/array.js
@@ -0,0 +1,24 @@
+let arr = [1,3,4,5]
+let news = arr.filter(function(val){
+    if(val>3)
+        { return true;}
+    else{
+        return false;
+    }
+
+    //console.log(news);
+    
+})
+console.log(news);
+
+
+var obj = {
+    name : " vaibhav ",
+    roll : 9,
+    dept : "computer science" 
+}
+//Object.freeze(obj);
+name = "anand";
+
+console.log(obj.name);
+
